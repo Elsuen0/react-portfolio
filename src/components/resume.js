c7c3906c456b0889c04f8040166625f1fda23cc3
Lucas Capart <lucas.capart80@gmail.com>
2023-07-06 18:18:14 +0200

    Update

---
+++ src/components/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const resume = () => {
+    return (
+        <div>
+            <a href="./CV_DW_Lucas_Capart.pdf"></a>
+        </div>
+    );
+};
+
+export default resume;
--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -92,7 +92,7 @@ export const ContactUs = () => {
             </Alert>
           </Col>
           <Col lg="5" className="mb-5">
-            <h3 className="color_sec py-4">Get in touch</h3>
+            <h3 className="color_sec py-4">Une question ?</h3>
             <address>
               <strong>Email :</strong>{" "}
               <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
